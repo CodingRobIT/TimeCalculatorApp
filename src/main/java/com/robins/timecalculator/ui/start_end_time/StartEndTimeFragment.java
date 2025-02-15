6341c59a61e1b15befe57ba81504c9c2e2697e8d
Robin <tmai02476@gmail.com>
2025-02-15 12:14:24 +0100

    more renaming to new start end time

--- src/main/java/com/robins/timecalculator/ui/start_end_time/StartEndTimeFragment.java
+++ src/main/java/com/robins/timecalculator/ui/start_end_time/StartEndTimeFragment.java
@@ -24,7 +24,7 @@ public class StartEndTimeFragment extends Fragment {
         binding = FragmentStartEndTimeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
+        final TextView textView = binding.textStartEndTime;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
